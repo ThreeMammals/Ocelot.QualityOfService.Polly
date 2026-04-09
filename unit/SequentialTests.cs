38763cae3db3463e6d5984f24f8c6ebefb475efe
Raman Maksimchuk <dotnet044@gmail.com>
2026-04-09 18:23:32 +0300

    Add unit tests

---
+++ unit/SequentialTests.cs
@@ -0,0 +1,8 @@
+﻿namespace Ocelot.QualityOfService.Polly.UnitTests;
+
+/// <summary>
+/// Apply <see cref="CollectionAttribute"/> to classes to disable parallelization.
+/// </summary>
+[CollectionDefinition(nameof(SequentialTests), DisableParallelization = true)]
+public class SequentialTests
+{ }
